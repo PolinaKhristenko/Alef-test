e31a74fbb16ccf1293d561e7eadbdc4a799f7348
PolinaTimofeeva <polleyw@mail.ru>
2022-04-26 15:40:22 +0300

    final

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,7 +6,7 @@
                 <nav class="header__body">
                     <a class="header__logo" href="/"><img src="../assets/img/logo.svg" alt="Логотип" class="header__logo"></a>
                     <div class="header__icons">
-                        <a class="header__icons-item">
+                        <a href="/" class="header__icons-item">
                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <circle cx="8.45945" cy="4.18919" r="3.68919" stroke="#333333"/>
                                 <path d="M0.5 15.3379H15.5" stroke="#333333"/>
@@ -14,7 +14,7 @@
                             </svg>
                         </a>
 
-                        <a class="header__icons-item">
+                        <a href="/" class="header__icons-item">
                             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M7.90002 3.0948C6.69981 -2.52108 -2.59119 1.90389 1.76037 7.60175C6.11193 13.2996 8.40002 15.4908 8.40002 15.4908" stroke="#333333"/>
                                 <path d="M7.89415 3.0948C9.09435 -2.52108 18.7304 1.90389 14.3789 7.60175C10.0273 13.2996 7.70001 15.5 7.70001 15.5" stroke="#333333"/>
@@ -22,14 +22,14 @@
                             </svg>
                         </a>
 
-                        <a class="header__icons-item">
+                        <a href="/" class="header__icons-item">
                             <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1.25468 7.56595C1.13958 6.11201 2.2884 4.86865 3.74689 4.86865H12.2531C13.7116 4.86865 14.8604 6.11201 14.7453 7.56595L14.2203 14.1975C14.1174 15.4977 13.0323 16.5002 11.7281 16.5002H4.27189C2.96769 16.5002 1.88261 15.4977 1.77968 14.1975L1.25468 7.56595Z" stroke="#333333"/>
                                 <path d="M3.99999 9.5C3.99999 9.5 3.78329 1 8.00001 1C12.2167 1 12 9.5 12 9.5" stroke="#333333"/>
                             </svg>
                         </a>
 
-                        <a class="header__icons-burger">
+                        <a href="/" class="header__icons-burger">
                             <transition name="fade" mode="out-in">
                                 <svg width="18" height="13" viewBox="0 0 18 13" fill="none" xmlns="http://www.w3.org/2000/svg"
                                 v-if="!show" @click="show = !show" key="menu">
